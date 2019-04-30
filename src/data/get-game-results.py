df15bd1ea9efa4e19958c46bf67504c81b664cb1
dfiorino <danielfiorino@gmail.com>
2019-04-29 22:27:55 -0500

    pep 8

--- src/data/get-game-results.py
+++ src/data/get-game-results.py
@@ -7,7 +7,7 @@ import argparse
 MAX_PAGES_BY_YEAR = {2018:9,2017:9,2016:10,2015:10,2014:7,2013:6,2012:4}
 CURRENT_YEAR = 2019
 
-def ParseArgs():
+def parse_args():
     """Setup command-line interface"""
     parser = argparse.ArgumentParser(description='Get all AUDL games.')
     
@@ -21,7 +21,7 @@ def ParseArgs():
                         help='Path and name of output CSV')
     return  parser.parse_args()
 
-def GetGamePageURLs(year):
+def get_game_page_urls(year):
     """Get URLs for all games of given year"""
     if year == CURRENT_YEAR:
         return [f'https://theaudl.com/league/schedule/week-{week_no}' for week_no in range(1,16)]
@@ -31,12 +31,12 @@ def GetGamePageURLs(year):
     return [f'https://theaudl.com/league/game?page={page_num}&year={year_id}' 
                                                                     for page_num in range(0,max_pages)]
 
-def GetSoup(url):
+def get_soup(url):
     """Return BS Soup Object"""
     result = requests.get(url)
     return BeautifulSoup(result.text,'lxml')
     
-def FormatGamesDict(year, matchup_links, start_times_places, score_pairs, team_name_pairs):
+def format_games_dict(year, matchup_links, start_times_places, score_pairs, team_name_pairs):
     """Format Game Info into Dictionary for Pandas DataFrame"""
     return [{'Year':year,
              'Matchup Link':mul,'Date Location':stp,
@@ -49,7 +49,7 @@ def FormatGamesDict(year, matchup_links, start_times_places, score_pairs, team_n
     
 def main():
     # Parse Argumnets
-    args = ParseArgs()
+    args = parse_args()
     output_csv = args.output_csv
     years = args.years
     
@@ -60,8 +60,8 @@ def main():
     for year_id, year in enumerate(years):
                     
     # Scrape Years of Games
-        for url in GetGamePageURLs(year):
-            soup = GetSoup(url) 
+        for url in get_game_page_urls(year):
+            soup = get_soup(url) 
                 
             if year == CURRENT_YEAR:
                 matchup_links = [i.find_all('a')[0].attrs['href'] for i in soup.find_all('span',attrs={'class':'audl-schedule-gc-link'})]
@@ -75,7 +75,7 @@ def main():
             score_pairs = [(i,j) for i,j in zip(scores[::2],scores[1::2])]
             team_name_pairs = [(i,j) for i,j in zip(team_names[::2],team_names[1::2])]
 
-            game_info_dicts +=FormatGamesDict(year, matchup_links, start_times_places,
+            game_info_dicts +=format_games_dict(year, matchup_links, start_times_places,
                                               score_pairs, team_name_pairs)
 
     games = pd.DataFrame(game_info_dicts)
@@ -87,6 +87,7 @@ def main():
     games['City'] = games['Date Location'].apply(lambda x : ' '.join(x.split(' ')[5:]) )
     games['Week'] = games['Date'].dt.strftime('%W').astype(int)
     games['Week'] = games.apply(lambda x : x.Week - games[games.Year==x.Year].Week.min() + 1,axis=1)
+    games['UniversalGameID'] = games.index
 
     games.sort_values('Date').reset_index(drop=True).to_csv(output_csv)
     
