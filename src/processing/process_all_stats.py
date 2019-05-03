904af8e2f550aa8e266cd48672a23931cfb45d97
Zane Rankin <zrankin@Zanes-MacBook-Pro.local>
2019-05-02 23:23:01 -0500

    major refactor of calculate_sum_stats, still have a little cleanup to do

--- src/processing/process_all_stats.py
+++ src/processing/process_all_stats.py
@@ -1,92 +1,37 @@
 import pandas as pd
-from src.processing.utils import load_data, team_index_vars, player_index_vars, DATA_DIR, initialize_stats, subset_gameplay
-from src.processing.stats import calculate_conversion_rates, calculate_additive_stats
+from src.processing.utils import load_data, team_index_vars, player_index_vars, DATA_DIR
+from src.processing.stats import calculate_conversion_rates, calculate_sum_stats
 from src.processing.stats import calculate_gameplay_stats, calculate_gameplay_stats_by_player
 
 team_count_indicators = ['Goals', 'Goals Against', 'Blocks', 'Drops', 'Throwaways', 'Turnovers']
 
 
 def make_team_indicators(df, index_vars=team_index_vars):
-    df = initialize_stats(df, ['Throwaways', 'Drops'])
-
-    # Conditions
-    offense = df['Event Type'] == 'Offense'
-    defense = df['Event Type'] == 'Defense'
-    callahan = df['Action'] == 'Callahan'
-    goal = (df['Action'] == 'Goal') | callahan
-    block = (df['Action'] == 'D') | callahan
-    drop = df['Action'] == 'Drop'
-    throwaway = (df['Action'] == 'Throwaway') | callahan
-
-
-    # Make row-wise indicators
-    df.loc[offense & goal, 'Goals'] = 1
-    df.loc[defense & goal, 'Goals Against'] = 1
-    df.loc[defense & block, 'Blocks'] = 1
-    df.loc[offense & drop, 'Drops'] = 1
-    df.loc[offense & (throwaway | callahan), 'Throwaways'] = 1
-    df = calculate_additive_stats(df)
-    df_wide1 = df.groupby(index_vars)[team_count_indicators].sum().reset_index()
+    df_counts = calculate_sum_stats(df, team_index_vars, entity='team')
 
     # Non row-wise indicators
     level_col = f'level_{len(index_vars)}'
-    df_wide2 = df.groupby(index_vars).apply(calculate_gameplay_stats).reset_index().drop(columns=level_col)
+    df_gameplay = df.groupby(index_vars).apply(calculate_gameplay_stats).reset_index().drop(columns=level_col)
 
-    df_wide = pd.merge(df_wide1, df_wide2, on=index_vars)
+    df_wide = pd.merge(df_counts, df_gameplay, on=index_vars)
     df_wide = calculate_conversion_rates(df_wide)
 
     return df_wide
 
 
 # TODO - clean up all lists of stat names
-PLAYER_SUM_INDICATORS = ['Completions', 'Assists', 'Hockey Assists', 'Throwaways', 'Receptions', 'Goals', 'Drops', 'Blocks', 'Callahans', 'Stalls', 'Callahans thrown']
+PLAYER_SUM_INDICATORS = ['Completions', 'Assists', 'Hockey Assists', 'Throwaways', 'Receptions', 'Goals', 'Drops',
+                         'Blocks', 'Callahans', 'Stalls', 'Callahans thrown']
 
 
 def make_player_indicators(df):
-    df = initialize_stats(df, PLAYER_SUM_INDICATORS)
-
-    # Actions
-    goal = df['Action'] == 'Goal'
-    block = df['Action'] == 'D'
-    drop = df['Action'] == 'Drop'
-    throwaway = df['Action'] == 'Throwaway'
-    completion = df['Action'] == 'Catch'
-    callahan = df['Action'] == 'Callahan'
-    stall = df['Action'] == 'Stall'
-
-    # Make passing row-wise indicators
-    df_p = df.copy()
-    df_p['player'] = df['Passer']
-    df_p.loc[completion, 'Completions'] = 1
-    df_p.loc[goal, 'Assists'] = 1
-    df_p.loc[(df_p.Action.shift(-1) == 'Goal') & (df_p['Event Type'] == 'Offense') &
-             (df_p['Event Type'].shift(-1) == 'Offense'), 'Hockey Assists'] = 1
-    df_p.loc[throwaway | callahan, 'Throwaways'] = 1
-    df_p.loc[callahan, 'Callahans thrown'] = 1
-    df_p.loc[stall, 'Stalls'] = 1
-
-    # Make receiving row-wise indicators
-    df_r = df.copy()
-    df_r['player'] = df['Receiver']
-    df_r.loc[completion, 'Receptions'] = 1
-    df_r.loc[goal, 'Goals'] = 1
-    df_r.loc[drop, 'Drops'] = 1
-
-    # Make defensive row-wise indicators
-    df_d = df.copy()
-    df_d['player'] = df['Defender']
-    df_d.loc[block | callahan, 'Blocks'] = 1
-    df_p.loc[callahan, 'Callahans'] = 1
-
-    df_all = pd.concat([df_p, df_r, df_d], sort=False)
-    df_wide1 = df_all.groupby(player_index_vars)[PLAYER_SUM_INDICATORS].sum().reset_index()
-    df_wide1 = calculate_additive_stats(df_wide1, entity='player')
+    df_counts = calculate_sum_stats(df, player_index_vars, entity='player')
 
     # Non row-wise indicators
-    df_wide2 = df.groupby(player_index_vars[:-1]).apply(calculate_gameplay_stats_by_player).reset_index().drop(
+    df_gameplay = df.groupby(player_index_vars[:-1]).apply(calculate_gameplay_stats_by_player).reset_index().drop(
         'level_2', axis=1)
 
-    df_wide = pd.merge(df_wide1, df_wide2, on=player_index_vars)
+    df_wide = pd.merge(df_counts, df_gameplay, on=player_index_vars, how='outer')
     df_wide = calculate_conversion_rates(df_wide)
 
     return df_wide
@@ -99,7 +44,6 @@ def clean_goals_for_sanke(df):
 
 
 if __name__ == '__main__':
-
     df = load_data()
 
     df_team = make_team_indicators(df)
--- src/processing/stats.py
+++ src/processing/stats.py
@@ -1,17 +1,72 @@
 import pandas as pd
-from .utils import list_players, count_points, count_possessions, count_games, subset_gameplay
+from .utils import list_players, count_points, count_possessions, count_games, subset_gameplay, initialize_stats
 
 
-def calculate_additive_stats(df, entity='team'):
-    df['Turnovers'] = df['Drops'] + df['Throwaways'] + df['Stalls']
-    df['Goals'] = df['Goals'] + df['Callahans']  # TODO - how to make this more clear
+def define_action_types(df):
+    goal = df['Action'].isin(['Goal', 'Callahan'])
+    block = df['Action'].isin(['D', 'Callahan'])
+    drop = df['Action'] == 'Drop'
+    throwaway = df['Action'].isin(['Throwaway', 'Callahan'])
+    catch = df['Action'].isin(['Catch', 'Goal'])
+    callahan = df['Action'] == 'Callahan'
+    stall = df['Action'] == 'Stall'
+
+    return [goal, block, drop, throwaway, catch, callahan, stall]
+
+
+def calculate_sum_stats(df, index_vars, entity='team'):
+    # TODO - figure out where to put lists of stats
+    cols = ['Assists', 'Hockey Assists', 'Throwaways', 'Completions', 'Catches', 'Goals', 'Drops', 'Blocks',
+            'Callahans', 'Stalls', 'Callahans Thrown']
+    initialize_stats(df, cols)
+
+    # TODO - could do via a melt?
+    df_t = df.rename(columns={'Passer': 'player'})
+    df_t['type'] = 'Passer'
+    df_r = df.rename(columns={'Receiver': 'player'})
+    df_r['type'] = 'Receiver'
+    df_d = df.rename(columns={'Defender': 'player'})
+    df_d['type'] = 'Defender'
+
+    df_long = pd.concat([df_t, df_r, df_d], sort=False)
+
+    passer = df_long.type == 'Passer'
+    receiver = df_long.type == 'Receiver'
+    defender = df_long.type == 'Defender'
+
+    goal, block, drop, throwaway, catch, callahan, stall = define_action_types(df_long)
+
+    offense = df_long['Event Type'] == "Offense"
+    defense = df_long['Event Type'] == "Defense"
+
+    # Thrower
+    df_long.loc[offense & passer & goal, 'Assists'] = 1
+    df_long.loc[offense & passer & (catch | goal), 'Completions'] = 1
+    df_long.loc[offense & passer & (df_long.Action.shift(-1) == 'Goal') & (
+                df_long['Event Type'].shift(-1) == 'Offense'), 'Hockey Assists'] = 1
+    df_long.loc[offense & passer & (throwaway | callahan), 'Throwaways'] = 1
+    df_long.loc[offense & passer & callahan, 'Callahans Thrown'] = 1
+    df_long.loc[offense & passer & stall, 'Stalls'] = 1
+
+    # Receiver
+    df_long.loc[offense & receiver & (catch | goal), 'Catches'] = 1
+    df_long.loc[offense & receiver & drop, 'Drops'] = 1
+
+    # Defender
+    df_long.loc[defense & defender & block, 'Blocks'] = 1
+    df_long.loc[defense & defender & callahan, 'Callahans'] = 1
+
+    # Combined
+    df_long.loc[(offense & receiver & goal) | (defense & defender & callahan), 'Goals'] = 1
+    df_long['Turnovers'] = df_long['Drops'] + df_long['Throwaways'] + df_long['Stalls']
+
     if entity == 'player':
         df['Plus_Minus'] = df.Goals + df.Assists + df.Blocks - df.Turnovers
-    return df
+
+    return df_long.groupby(index_vars)[cols].sum().reset_index()
 
 
 def calculate_gameplay_stats(df):
-
     df_o = subset_gameplay(df, line="O", remove_cessation=True)
     df_d = subset_gameplay(df, line="D", remove_cessation=True)
 
@@ -21,14 +76,19 @@ def calculate_gameplay_stats(df):
                          'D Possessions Played': count_possessions(df_d),
                          'Holds': count_points(subset_gameplay(df_o, event_type='Offense', action='Goal')),
                          'Breaks': count_points(subset_gameplay(df_d, event_type='Offense', action='Goal')),
-                         'Points Won': count_points(subset_gameplay(df, event_type='Offense', action='Goal')),  # TODO - could sum O and D points scored
+                         'Points Won': count_points(subset_gameplay(df, event_type='Offense', action='Goal')),
+                         # TODO - could sum O and D points scored
                          'Games Played': count_games(df),
                          'Points Played': count_points(df),  # TODO - could sum O and D points
                          'Possessions Played': count_possessions(df),  # TODO - could sum O and D possessions
-                         'Opponent Possessions Played (O-Line)': count_possessions(subset_gameplay(df_o, event_type='Defense')),
-                         'Opponent Possessions Played (D-Line)': count_possessions(subset_gameplay(df_d, event_type='Defense')),
-                         'O Points Lost': count_points(subset_gameplay(df_o, event_type='Defense', action='Goal')),  # TODO - could do O points - Holds
-                         'D Points Lost': count_points(subset_gameplay(df_d, event_type='Defense', action='Goal')),  # TODO - could do D points - Breaks
+                         'Opponent Possessions Played (O-Line)': count_possessions(
+                             subset_gameplay(df_o, event_type='Defense')),
+                         'Opponent Possessions Played (D-Line)': count_possessions(
+                             subset_gameplay(df_d, event_type='Defense')),
+                         'O Points Lost': count_points(subset_gameplay(df_o, event_type='Defense', action='Goal')),
+                         # TODO - could do O points - Holds
+                         'D Points Lost': count_points(subset_gameplay(df_d, event_type='Defense', action='Goal')),
+                         # TODO - could do D points - Breaks
                          }, index=[0])
 
 
