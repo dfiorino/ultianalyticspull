ac91a797f197fa216337fd253be923eeeffd79ae
dfiorino <danielfiorino@gmail.com>
2019-06-02 12:58:18 -0500

    updates for week 9

--- src/data/get-weekly-active-rosters.py
+++ src/data/get-weekly-active-rosters.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import os
 
 somelist=[]
-for week_no in range(1,4): # only for weeks 1&2 of 2019 so far
+for week_no in range(1,10): # only for weeks 1&2 of 2019 so far
     
     url = f'https://theaudl.com/league/news/2019-audl-active-rosters-week-{week_no}'
     result = requests.get(url)
