ac144de4248f59753d8ea93edd66664e4c6721c7
dfiorino <danielfiorino@gmail.com>
2019-06-23 22:42:54 -0500

    verions up!

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='0.1.0',
-    description='Tools to pull and process data from the American Ultimate Disc League',
+    version='0.2.0',
+    description='Tools to pull and process data from the American Ultimate Disc League and Premier Ultimate League',
     author="Dan Fiorino <danielfiorino@gmail.com>, Zane Rankin <zwrankin@gmail.com>",
     license='GPL',
 )
