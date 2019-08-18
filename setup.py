53df6c5af9298e7fda4c3afe00b5fc7fc0e734e3
dfiorino <danielfiorino@gmail.com>
2019-08-18 01:16:33 -0500

    I like to move it move. package prep

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name='ultianalyticspull',
     packages=find_packages(),
-    version='0.2.0',
+    include_package_data=True,
+    python_requires='>3.5.2',
+    version='0.3.0',
     description='Tools to pull and process data from the American Ultimate Disc League and Premier Ultimate League',
     author="Dan Fiorino <danielfiorino@gmail.com>, Zane Rankin <zwrankin@gmail.com>",
     license='GPL',
+    entry_points={'console_scripts': [
+                'aggregate_stats = src.processing.process_all_stats.py:main']
+                }
 )
