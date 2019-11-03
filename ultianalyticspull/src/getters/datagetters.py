bd2e73f83b6136a3b17199d555786ff54ad7978e
dfiorino <danielfiorino@gmail.com>
2019-11-03 12:24:28 -0600

    simplify scapers and output of scrapers code

---
+++ ultianalyticspull/src/getters/datagetters.py
@@ -0,0 +1,16 @@
+# Get Handmade data and data paths
+import importlib.resources as import_resources
+import pandas as pd
+
+def get_audl_teams_dataframe():
+    with import_resources.path('ultianalyticspull.data.audl.supplemental', 'audl_teams.csv') as audl_teams:
+        return pd.read_csv(audl_teams)
+
+
+def get_audl_logos_path():
+    with import_resources.path('ultianalyticspull.images.audl-logos', '')as images_path:
+        return images_path
+
+def get_audl_supplemental_data_path():
+    with import_resources.path('ultianalyticspull.data.audl.supplemental', '') as audl_data_path:
+        return audl_data_path
---
+++ ultianalyticspull/src/scraping/out.py
@@ -0,0 +1,62 @@
+from ultianalyticspull.src.getters.datagetters as getters
+from ultianalyticspull.src.scrapers.scrape as scrape
+
+def output_audl_weekly_active_rosters():
+    # Get Data/Paths
+    audl_data_path = getters.get_audl_logos_path()
+    audl_active = scrape.get_audl_weekly_active_rosters()
+    # Output
+    output_csv = f'{audl_data_path}/audl_weekly_active_rosters.csv'
+    audl_active.to_csv(output_csv)
+    print(f"Wrote {output_csv}")
+
+def output_audl_rosters_from_stats_page():
+    # Get Data/Paths
+    audl_data_path = getters.get_audl_supplemental_data_path()
+    audlstats_players = scrape.get_audl_rosters_from_stats_page()
+    # Output
+    output_csv = f"{audl_data_path}/audl_players_from_stats_page.csv"
+    audlstats_players.to_csv(output_csv)
+    print(f'Wrote {output_csv}')
+
+def output_audl_game_results(years=[2012,2013,2014,2015,2016,2017,2018,2019]):
+    # Get Data/Paths
+    audl_data_path = getters.get_audl_supplemental_data_path()
+    games = get_audl_game_results(years=years):
+    # Output
+    output_csv = f"{audl_data_path}/audl_games.csv"
+    games.sort_values('Date').reset_index(drop=True).to_csv(output_csv)
+    print(f"Wrote {output_csv}")
+
+def output_audl_current_rosters():
+
+    # Get Data/Paths
+    audl_data_path = getters.get_audl_supplemental_data_path()
+    audldotcom_rosters = get_audl_current_rosters()
+    # Output
+    output_csv = f"{audl_data_path}/2019_rosters.csv'"
+    audldotcom_rosters.to_csv(output_csv)
+    print(f"Wrote {output_csv}")
+
+def output_audl_team_logos():
+    # Get Data/Paths
+    audl_data_path = getters.get_audl_logos_path()
+    team_info = getters.get_audl_teams_dataframe()
+
+    # Create Lookup
+    abbrev2full = pd.Series(teaminfo[teaminfo['Active']]['Teamname'].values,
+                            index=teaminfo[teaminfo['Active']]['Team Abv'].values).to_dict()
+
+    # Output PNGs
+    for abbr in teaminfo[teaminfo['Active']]['Team Abv']:
+        url = f'https://theaudl.com/sites/default/files/logo-team-{abbr}.png'
+        output_png = f'{audl_data_path}/logo-team-{abbr}.png'
+        urllib.request.urlretrieve(url,output_png)
+        print(f"Wrote {output_png}")
+
+def output_scraped_data():
+    output_audl_weekly_active_rosters()
+    output_audl_rosters_from_stats_page()
+    output_audl_game_results()
+    output_audl_current_rosters()
+    output_audl_team_logos()
