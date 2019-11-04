ed149e9d1e1902c5201919ee3ea2682981599786
dfiorino <danielfiorino@gmail.com>
2019-11-04 00:16:02 -0600

    remove old code

--- ultianalyticspull/src/core/utils.py
+++ ultianalyticspull/src/core/utils.py
@@ -2,9 +2,6 @@ import pandas as pd
 import numpy as np
 import glob
 import csv
-import os
-
-__this_dir__ = os.path.dirname(os.path.realpath(__file__))
 
 def csv2dataframe(filename : str):
     """Read CSV as Pandas DataFrame but deal with inconsistent use of commas in CSV"""
@@ -38,35 +35,3 @@ def make_game_over_line(df_in):
     gameoverline['Lateral Distance']        =np.nan
     gameoverline['Toward Our Goal Distance']=np.nan
     return gameoverline.to_frame().T
-
-def audl_data(years : list = [2014,2015,2016,2017,2018,2019], processed : bool = True):
-    """Get throw-by-throw data for each team. Raw data from Ultianalytics has been processed by AUDL Pull"""
-    file_type = 'processed' if processed else 'raw'
-    file_list = [file for yr in years for file in glob.glob(f'{__this_dir__}/../data/{file_type}/audl/{yr}/*csv')]
-    return pd.concat([pd.read_csv(i) for i in file_list])
-
-def pul_data(years : list = [2019], processed : bool = True):
-    """Get throw-by-throw data for each team. Raw data from Ultianalytics has been processed by PUL Pull"""
-    file_type = 'processed' if processed else 'raw'
-    file_list = [file for yr in years for file in glob.glob(f'{__this_dir__}/../data/{file_type}/pul/{yr}/*csv')]
-    return pd.concat([pd.read_csv(i) for i in file_list])
-
-def get_audl_games():
-    """Get DataFrame of all AUDL games"""
-    return pd.read_csv(f'{__this_dir__}/../data/supplemental/audl/audl_games.csv')
-
-# def get_pul_games():
-#     """Get DataFrame of all PUL games"""
-#     return pd.read_csv(f'{__this_dir__}/../data/supplemental/pul/pul_games.csv')
-
-def get_audl_teams():
-    """Get DataFrame of AUDL Team information."""
-    return pd.read_csv(f'{__this_dir__}/../data/supplemental/audl/audl_teams.csv')
-
-def get_pul_teams():
-    """Get DataFrame of PUL Team information."""
-    return pd.read_csv(f'{__this_dir__}/../data/supplemental/pul/pul_teams.csv')
-
-def get_audl_weekly_rosters():
-    """Get DataFrame of weekly AUDL active rosters."""
-    return pd.read_csv(f'{__this_dir__}/../data/supplemental/audl/audl_weekly_active_rosters.csv')
