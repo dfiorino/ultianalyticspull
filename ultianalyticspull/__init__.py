1bbf10636e37c3564d7500501bdcd2b9f2c7f9fa
Dan Fiorino <danielfiorino@gmail.com>
2020-02-16 20:18:18 -0600

    Patch docs examples (#14)
    * Fix api
    
    * Update Example.ipynb
    
    * bug fix
    
    * remove old way of storing output inside library. boo!

--- ultianalyticspull/__init__.py
+++ ultianalyticspull/__init__.py
@@ -2,7 +2,6 @@ from ultianalyticspull.src.processing.process_all_stats import *
 from ultianalyticspull.src.processing.stats import *
 from ultianalyticspull.src.processing.utils import *
 
-from ultianalyticspull.src.core.individualstats import *
 from ultianalyticspull.src.core.opponents import *
 from ultianalyticspull.src.core.huddlers import *
 from ultianalyticspull.src.core.pullers import *
